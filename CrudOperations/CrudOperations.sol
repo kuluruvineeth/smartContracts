d268e018c3bc91f4e6db260e7c37e7d37a3f92d5
kuluruvineeth <kuluruvineeth8623@gmail.com>
2021-12-31 13:54:18 +0530

    CRUD operations contract

---
+++ CrudOperations/CrudOperations.sol
@@ -0,0 +1,43 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.7.0 <0.9.0;
+
+contract CrudOperations {
+
+    struct User {
+        uint id;
+        string name;
+    }
+
+    User[] public users;
+    uint public nextId = 1;
+
+    function create(string memory name) public {
+        users.push(User(nextId,name));
+        nextId++;
+    }
+
+    function read(uint id) view public returns(uint,string memory){
+        uint i = find(id);
+        return(users[i].id,users[i].name);
+    }
+
+    function update(uint id,string memory name) public {
+        uint i = find(id);
+        users[i].name = name;
+    } 
+
+    function destroy(uint id) public {
+        uint i = find(id);
+        delete users[i];
+    }
+
+    function find(uint id) view internal returns(uint){
+        for(uint i=0;i<users.length;i++){
+            if(users[i].id == id){
+                return i;
+            }
+        }
+        revert('User does not exist!');
+    }
+}
